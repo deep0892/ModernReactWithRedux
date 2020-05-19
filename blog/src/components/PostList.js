aca7af6fa8f556207c088ba68fc5251107f53952
Deepankar <deepankar080892@gmail.com>
2020-05-19 22:22:10 +0200

    Async actions with redux thunk

---
+++ blog/src/components/PostList.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { fetchPosts } from "../actions";
+
+class PostList extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return <div>PostList</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
