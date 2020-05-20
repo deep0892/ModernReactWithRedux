6d15aebee6298c2df9ccd3eddee1d93846d06223
Deepankar <deepankar080892@gmail.com>
2020-05-20 17:15:11 +0200

    Remove Secret key

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.unsplash.com",
     headers: {
-        Authorization: "Client-ID 63DhDw-igbg5gc_55XF4olRzSvxgs91HdO-v0lykGYw",
+        Authorization: "Client-ID ",
     },
 });
