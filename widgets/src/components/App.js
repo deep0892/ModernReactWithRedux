c32c6a73a8755d1227601ddb6286a6d32a69eb84
Deepankar <deepankar080892@gmail.com>
2020-07-21 03:10:04 +0530

    bug fix in case of toggle and event remnoval

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -38,16 +38,26 @@ const options = [
 
 const App = () => {
   const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
 
   return (
     <div className="ui container">
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <Dropdown
-        selected={selected}
-        onSelectedChange={setSelected}
-        options={options}
-      />
+      <button
+        onClick={() => {
+          setShowDropdown(!showDropdown);
+        }}
+      >
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
