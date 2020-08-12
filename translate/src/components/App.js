fface7afd9852747ff4a644d7028f7196df01ec4
Deepankar <deepankar080892@gmail.com>
2020-08-12 11:53:55 +0530

    Component setup

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return <div className="ui container">App</div>;
+import UserCreate from './UserCreate';
+class App extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange('english')}
+          ></i>
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange('dutch')}
+          ></i>
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
 }
 
 export default App;
