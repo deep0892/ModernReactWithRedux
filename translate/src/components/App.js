b09c32c28f4831863a75c28b0d7cf652886af905
Deepankar <deepankar080892@gmail.com>
2020-08-12 13:19:03 +0530

    THe Context Provider

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+
 class App extends Component {
   state = { language: 'english' };
 
@@ -22,7 +24,9 @@ class App extends Component {
             onClick={() => this.onLanguageChange('dutch')}
           ></i>
         </div>
-        <UserCreate />
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
       </div>
     );
   }
