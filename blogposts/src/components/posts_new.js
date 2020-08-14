e37844209821465ed5ad37b592ca2eafbb945ccb
Deepankar <deepankar080892@gmail.com>
2020-08-15 01:37:19 +0530

    A React Route Gotcha

---
+++ blogposts/src/components/posts_new.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import connect from 'react-redux';
+
+class PostsNEW extends Component {
+  render() {
+    return <div>PostsNEW</div>;
+  }
+}
+
+// export default connect(null, {})(PostsNEW);
+export default PostsNEW;
